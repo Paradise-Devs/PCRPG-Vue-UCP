f574e2873046207735f58c4403947bc08e58474a
Lós <los799@live.com>
2018-01-20 18:47:30 -0200

    Adiciona rota para erro 404

---
+++ src/pages/errors/404.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-container>
+        <h1>404</h1>
+        <h3>Não encontramos a página que você está procurando =(</h3>
+        <router-link to="/" class="button primary">Voltar a página inicial</router-link>
+    </b-container>
+</template>
+
+<style scoped>
+    .container {
+        min-height: 62.5vh;
+        padding: 30px;
+        text-align: center;
+    }
+
+    .container h1 {
+        text-align: center;
+        font-size: 900%;
+    }
+
+    .container h3 {
+        text-align: center;
+        margin-bottom: 30px;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Router from 'vue-router'
 import IndexView from '@/pages/Index'
 import PatchnotesView from '@/pages/Patchnotes'
 import DevelopmentView from '@/pages/Development'
+import NotFoundView from '@/pages/errors/404'
 
 Vue.use(Router)
 
@@ -12,20 +13,9 @@ Vue.use(Router)
 export default new Router({
 	mode: 'history',
 	routes: [
-		{
-			path: '/',
-			name: 'Index',
-			component: IndexView
-		},
-		{
-			path: '/patchnotes',
-			name: 'Patchnotes',
-			component: PatchnotesView
-		},
-		{
-			path: '/dev',
-			name: 'Development',
-			component: DevelopmentView
-		}
+		{ path: '/', 			name: 'Index', 			component: IndexView },
+		{ path: '/patchnotes', 	name: 'Patchnotes', 	component: PatchnotesView },
+		{ path: '/dev', 		name: 'Development', 	component: DevelopmentView },
+		{ path: '*', 			name: '404', 			component: NotFoundView },
 	]
 })
