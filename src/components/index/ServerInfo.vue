ba9875e715b6475fea2a340c0a03f4d6ca6de01a
Lós <los799@live.com>
2018-01-27 19:54:18 -0200

    Melhora a forma de mostrar o servidor online/offline

--- src/components/index/ServerInfo.vue
+++ src/components/index/ServerInfo.vue
@@ -2,17 +2,19 @@
     <b-container class="server-info">
         <vue-spinner :loading="serverInfoLoading" color="#303846" size="10px"></vue-spinner>
         <b-row class="loaded" v-if="server.processed">
-            <b-col cols="2">
-                <span class="status online" v-if="server.online">Servidor Online</span>
-                <span class="status offline" v-else>Servidor Offline</span>
-            </b-col>
-            <b-col cols="8" class="name" v-if="server.online">
-                <span>{{ server.serverName }}</span>
-            </b-col>
-            <b-col cols="2" class="players" v-if="server.online">
-                <span>Jogadores: {{ server.currentPlayers }}/{{ server.maxPlayers}}</span>
-            </b-col>
-        </b-row>
+			<b-col cols="2" class="status online" v-if="server.online">
+				Servidor Online
+			</b-col>
+			<b-col cols="8" class="name" v-if="server.online">
+				<span>{{ server.serverName }}</span>
+			</b-col>
+			<b-col cols="2" class="players" v-if="server.online">
+				<span>Jogadores: {{ server.currentPlayers }}/{{ server.maxPlayers}}</span>
+			</b-col>
+			<b-col cols="12" class="status offline" v-if="!server.online">
+				Servidor Offline
+			</b-col>
+		</b-row>
     </b-container>
 </template>
 
@@ -43,7 +45,7 @@
 				axios.get(serverApi)
 				.then(response => {
 					for (let i = 0; i < response.data.list.length; i++) {
-						if (response.data.list[i].resolvedIP == "176.57.141.110:4499") {
+						if (response.data.list[i].resolvedIP == "176.57.141.111:4499") {
 							this.server = response.data.list[i];
 							this.foundServer = true;
 							this.server.processed = true;
@@ -76,12 +78,12 @@
     }
 </script>
 
-<style>
+<style scoped>
 	.server-info {
 		margin: 30px 0px;
 		padding: 10px;
-		min-height: 47px;
-		max-height: 47px;
+		min-height: 41px;
+		max-height: 41px;
 		border-radius: 5px;
 		border: 1px solid #303846;
 	}
@@ -94,8 +96,10 @@
 		color: #00e676;
 	}
 
-	.server-info .status.online {
-		color: #00e676;
+	.server-info .status.offline {
+		width: 100%;
+		text-align: center;
+		color: #ff5252;
 	}
 
 	.server-info .name {
