73493a17a1c57c18a01a05d7018baa26b9466364
Lucas Souza <lucas.souza.ext@wunderman.com>
2018-03-27 15:14:46 -0300

    Migração de componentes

--- src/pages/userProfile/Home.vue
+++ src/pages/userProfile/Home.vue
@@ -1,17 +1,35 @@
 <template>
-    <b-row>
-        <char 
-            v-for="char in charsData" 
-            :key="char.id"
-            :char='char'
-        />
-    </b-row>
+	<div>
+		<b-row>
+			<char
+				v-for="char in charsData"
+				:key="char.id"
+				:char='char'
+			/>
+		</b-row>
+		<b-row class="charnav">
+			<b-col cols="12">
+				<b-tabs>
+					<b-tab title="Fórum" active>
+						<forumlog/>
+					</b-tab>
+					<b-tab title="Servidor">
+
+					</b-tab>
+					<b-tab title="Histórico">
+
+					</b-tab>
+				</b-tabs>
+			</b-col>
+		</b-row>
+	</div>
 </template>
 
 <script>
     import axios from 'axios';
 
     import char from '@/components/profile/CharCard.vue'
+	import forumlog from '@/components/profile/ForumLog.vue'
 
     import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
@@ -47,7 +65,38 @@
         },
         components: {
             'vue-spinner': PulseLoader,
-            char
+            char,
+			forumlog
         }
     }
 </script>
+
+<style>
+	.charnav {
+		margin-top: 20px;
+	}
+
+	.charnav .nav {
+		border-color: #1B2028;
+	}
+
+	.charnav .nav-tabs .nav-link.active, .nav-tabs .nav-item.show .nav-link {
+		background-color: #1B2028;
+		border-color: #1B2028;
+		color: #6c7d93;
+	}
+
+	.charnav .nav-tabs .nav-link {
+		color: #303846;
+	}
+
+	.charnav .nav-tabs .nav-item {
+		width: 33.33%;
+		text-align: center;
+	}
+
+	.charnav .nav-tabs .nav-link:focus:not(.active), .charnav .nav-tabs .nav-link:hover:not(.active) {
+		border-color: #1B2028;
+		background-color: #161A20;
+	}
+</style>
