9887bab01393548d8e97660bc31b71123ba68ab3
Lós <los799@live.com>
2018-05-06 13:40:03 -0300

    Componetiza o avatar do usuário no UCP

---
+++ src/components/user-profile/avatar.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="comp__userAvatar">
+        <img :src="user.forumAtt.attributes.avatarUrl" v-if="user.forumAtt.attributes.avatarUrl != null" />
+        <div class="comp__userAvatar--empty" v-else> ? </div>
+        <div class="comp__userAvatar__uploader" v-if="editable">
+            <label for='newAvatarField' class="button"><fa :icon="['fas', 'upload']" /></label>
+            <input id='newAvatarField' type='file' accept="image/jpeg, image/png" @change="onFileChanged">
+        </div>
+        <span 
+            class="comp__userAvatar__uploader__status" 
+            :class="{'comp__userAvatar__uploader__status--success': processingSuccess, 'comp__userAvatar__uploader__status--error': processingError }"
+            v-if="processingStep != 2 && processingStep > 0 && editable"
+        > 
+            <span v-if="processingStep == 1">Iniciando</span>
+            <span v-if="processingStep == 3">Finalizando</span>
+            <span v-if="processingStep == 4">Atualizando</span>
+            <span v-if="processingStep == 5">Erro!</span>
+        </span>
+        <b-progress class="comp__userAvatar__uploader__status" :max="processingMax" v-else-if="processingStep == 2">
+            <b-progress-bar :value="processingPerc">
+               {{ animatedProcessingValue }}
+            </b-progress-bar>
+        </b-progress>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import { store } from '@/vuex/store';
+    import fontawesome from '@fortawesome/vue-fontawesome';
+    import { TweenMax } from 'gsap'
+
+    import { upload } from '@fortawesome/fontawesome-free-solid';
+
+	var usersBaseURI = 'https://forum.pc-rpg.com.br/api/users/';
+
+    export default {
+        props: {
+            user: Object,
+            editable: Boolean
+        },
+        data() {
+			return {
+                loading: true,
+                editable: false,
+                processingStep: 0,
+                processingPerc: 0,
+                processingTweenedPerc: 0,
+                processingMax: 100,
+                processingSuccess: false,
+                processingError: false
+            }
+        },
+        watch: {
+            processingPerc: function(newValue) {
+                TweenLite.to(this.$data, 0.5, { processingTweenedPerc: newValue });
+            }
+        },
+        computed: {
+            animatedProcessingValue: function() {
+                return this.processingTweenedPerc.toFixed();
+            }
+        },
+        methods: {
+            onFileChanged(event) {
+                this.user.forumAtt.attributes.avatarUrl = URL.createObjectURL(event.target.files[0]);
+                this.uploadAvatar(event.target.files[0]);
+            },
+            uploadAvatar: function(file) {
+                this.processingStep = 1;
+                const formData = new FormData();
+                formData.append('avatar', file, file.name);
+                let userId = this.user.forumAtt.id;
+                let tot = 0;
+                let perc = 0;
+
+                axios.post(usersBaseURI + userId + '/avatar', formData, {
+                    headers: {
+                        "Authorization": "Token " + store.getters.getMasterToken + 'userId=' + userId
+                    },
+                    onUploadProgress: ProgressEvent => {
+                        perc = (ProgressEvent.loaded / ProgressEvent.total) * 100;
+                        console.log(perc);
+                        this.processingStep = 2;
+                        this.processingPerc = perc;
+
+                        if(perc.toFixed() == 100) {
+                            this.processingStep = 3;
+                            this.processingSuccess = true;
+                        }
+                    }
+                })
+                .then(response => {
+                    this.user.forumAtt.attributes.avatarUrl = response.data.data.attributes.avatarUrl;
+                    this.processingStep = 4;
+                    this.processingSuccess = true;
+
+                    store.dispatch('setData', this.user).then(() => {
+                        location.reload();
+                    });
+                })
+                .catch(error => {
+                    this.processingStep = 5;
+                    console.log(error);
+                })
+            }
+        },
+        components: {
+            'fa': fontawesome
+        }
+    }
+</script>
