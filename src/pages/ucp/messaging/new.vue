db969a503181561f09d646af6bc71b3e07ce6bb2
Lucas Souza <lucas.souza.ext@wunderman.com>
2018-06-14 16:43:02 -0300

    Progresso em mensagens

--- src/pages/ucp/messaging/new.vue
+++ src/pages/ucp/messaging/new.vue
@@ -1,66 +1,65 @@
 <template>
-    <div class="page__ucp--message-new">
-        <b-form class="new_message" @submit.prevent="SendMessage">
-            <div class="preview markdown" v-html="marked"> {{ text | marked }} </div>
-            <div class="inputs">
-                <b-form-group>
-                    <b-form-input type="email" v-model="title" required placeholder="Assunto da mensagem"/>
-                </b-form-group>
-                <b-form-group>
-                    <b-form-input type="email" v-model="title" required placeholder="Destinatário" />
-                </b-form-group>
-                <b-form-group description="Você pode utilizar <a href='http://commonmark.org/help/' target='_blank'>Markdown</a> para formatar o seu texto">
-                    <textarea placeholder="Escreva aqui" v-model="text" class="new_message__textarea"/>
-                </b-form-group>
-                <b-form-group>
-                    <b-button variant="secondary">Cancelar</b-button>
-                    <b-button variant="primary" class="submit">Enviar</b-button>
-                </b-form-group>
-            </div>
-        </b-form>
-    </div>
+	<div class="page__ucp--message-new">
+		<b-form class="new_message" @submit.prevent="SendMessage">
+			<div class="preview markdown" v-html="marked"></div>
+			<div class="inputs">
+				<b-form-group>
+					<b-form-input type="email" v-model="title" required placeholder="Assunto da mensagem"/>
+				</b-form-group>
+				<b-form-group>
+					<b-form-input type="email" v-model="dest" required placeholder="Destinatário" />
+				</b-form-group>
+				<b-form-group description="Você pode utilizar <a href='http://commonmark.org/help/' target='_blank'>Markdown</a> para formatar o seu texto">
+					<textarea placeholder="Escreva aqui" v-model="text" class="new_message__textarea"/>
+				</b-form-group>
+				<b-form-group>
+					<b-button variant="secondary">Cancelar</b-button>
+					<b-button variant="primary" class="submit">Enviar</b-button>
+				</b-form-group>
+			</div>
+		</b-form>
+	</div>
 </template>
 
 <script>
-    import { store } from '@/vuex/store'
-    import MessagingService from '@/services/messaging'
-    import marked from 'marked';
+import { store } from "@/vuex/store";
+import MessagingService from "@/services/messaging";
+import marked from "marked";
 
-    export default {
-        data() {
-            return {
-                user: store.state.user,
-                text: '',
-                title: ''
-            }
-        },
-        computed: {
-            marked: function () {
-                return marked(this.text, { sanitize: true })
-            }
-        },
-        methods: {
-            SendMessage: function() {
-                let sender, receiver;
-
-                MessagingService.sendMessage(sender, receiver, title, text)
-                .then(res => {
-
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-            }
-        },
-        mounted() {
-			store.watch(
-				(state)=>{
-					return store.getters.getUserData
-				},
-				(oldValue, newValue)=>{
-					this.user = store.state.user;
-				}
-            )
-        }
+export default {
+  data() {
+    return {
+      user: store.state.user,
+      text: "",
+      title: "",
+      dest: ""
+    };
+  },
+  computed: {
+    marked: function() {
+      return marked(this.text, { sanitize: true });
     }
+  },
+  methods: {
+    SendMessage: function() {
+      let sender, receiver;
+
+      MessagingService.sendMessage(sender, receiver, title, text)
+        .then(res => {})
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  },
+  mounted() {
+    store.watch(
+      state => {
+        return store.getters.getUserData;
+      },
+      (oldValue, newValue) => {
+        this.user = store.state.user;
+      }
+    );
+  }
+};
 </script>
--- src/services/messaging.js
+++ src/services/messaging.js
@@ -1,49 +1,61 @@
-import Vue from 'vue';
-import axios from 'axios';
+import Vue from "vue";
+import axios from "axios";
 
 var baseUri;
 
-if((location.hostname != "pc-rpg.com.br") && (location.hostname != "www.pc-rpg.com.br")) {
-    baseUri = 'http://dev.pc-rpg.com.br:3000/api/v1/messages/';
+if (
+	location.hostname != "pc-rpg.com.br" &&
+	location.hostname != "www.pc-rpg.com.br"
+) {
+	baseUri = "http://dev.pc-rpg.com.br:3000/api/v1/messages/";
 } else {
-    baseUri = 'https://prod.pc-rpg.com.br:3000/api/v1/messages/';
+	baseUri = "https://prod.pc-rpg.com.br:3000/api/v1/messages/";
 }
 
-var auth = { "Authorization": "Bearer " + localStorage.getItem('token') }
+var auth = { Authorization: "Bearer " + localStorage.getItem("token") };
 
 export default {
-    //send
-    sendMessage(from, to, subject, body) {
-        let e = new Date();
-        let date = e.toISOString();
+	//send
+	sendMessage(from, to, subject, body) {
+		let e = new Date();
+		let date = e.toISOString();
 
-        return axios.post(baseUri + 'send', 
-            { from: from, to: to, subject: subject, body: body, sendDate: date },
-            auth
-        );
-    },
+		return axios.post(
+			baseUri + "send",
+			{ from: from, to: to, subject: subject, body: body, sendDate: date },
+			auth
+		);
+	},
 
-    //get messages
-    getMessageData(msgid) {
-        return axios.get(baseUri + msgid, { headers: auth });
-    },
-    getMessagesTo(user) {
-        return axios.get(baseUri + 'to/' + user, { headers: auth });
-    },
-    getMessagesFrom(user) {
-        return axios.get(baseUri + 'from/' + user, { headers: auth });
-    },
-    getDeletedMessages(user) {
-        return axios.get(baseUri + 'trash/' + user, { headers: auth });
-    },
+	//get messages
+	getMessageData(msgid) {
+		return axios.get(baseUri + msgid, { headers: auth });
+	},
+	getMessagesTo(user) {
+		return axios.get(baseUri + "to/" + user, { headers: auth });
+	},
+	getMessagesFrom(user) {
+		return axios.get(baseUri + "from/" + user, { headers: auth });
+	},
+	getDeletedMessages(user) {
+		return axios.get(baseUri + "trash/" + user, { headers: auth });
+	},
 
-    //Update
-    markMessageAsRead(msgid) {
-        return axios.post(baseUri + 'markAsRead', { messageId: msgid }, { headers: auth });
-    },
+	//Update
+	markMessageAsRead(msgid) {
+		return axios.post(
+			baseUri + "markAsRead",
+			{ messageId: msgid },
+			{ headers: auth }
+		);
+	},
 
-    //delete
-    deleteMessage(messagesid) {
-        return axios.post(baseUri + 'delete', { messages: [ messagesid ] }, { headers: auth } );
-    }
-}
+	//delete
+	deleteMessage(messagesid) {
+		return axios.post(
+			baseUri + "delete",
+			{ messages: [messagesid] },
+			{ headers: auth }
+		);
+	}
+};
