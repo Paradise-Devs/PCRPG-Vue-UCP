4aca8356543adfd0dcaac381f1eeae5ed064b134
Lós <los799@live.com>
2018-06-16 14:03:28 -0300

    Adiciona estilos de validação em field textarea

--- src/pages/ucp/messaging/new.vue
+++ src/pages/ucp/messaging/new.vue
@@ -4,13 +4,25 @@
 			<div class="preview markdown" v-html="marked"></div>
 			<div class="inputs">
 				<b-form-group>
-					<b-form-input type="email" v-model="title" required placeholder="Assunto da mensagem"/>
+					<b-form-input 
+						type="email" 
+						v-model="title" 
+						placeholder="Assunto da mensagem"
+					/>
 				</b-form-group>
 				<b-form-group>
-					<b-form-input type="email" v-model="dest" required placeholder="Destinatário" />
+					<b-form-input 
+						type="email"
+						v-model="dest"
+						required
+						placeholder="Destinatário"
+					/>
 				</b-form-group>
 				<b-form-group description="Você pode utilizar <a href='http://commonmark.org/help/' target='_blank'>Markdown</a> para formatar o seu texto">
-					<textarea placeholder="Escreva aqui" v-model="text" class="new_message__textarea"/>
+					<textarea 
+						placeholder="Escreva aqui"
+						v-model="text"
+					/>
 				</b-form-group>
 				<b-form-group>
 					<b-button variant="secondary">Cancelar</b-button>
@@ -22,44 +34,46 @@
 </template>
 
 <script>
-import { store } from "@/vuex/store";
-import MessagingService from "@/services/messaging";
-import marked from "marked";
+	import { store } from "@/vuex/store";
+	import MessagingService from "@/services/messaging";
+	import marked from "marked";
 
-export default {
-  data() {
-    return {
-      user: store.state.user,
-      text: "",
-      title: "",
-      dest: ""
-    };
-  },
-  computed: {
-    marked: function() {
-      return marked(this.text, { sanitize: true });
-    }
-  },
-  methods: {
-    SendMessage: function() {
-      let sender, receiver;
+	export default {
+	data() {
+		return {
+		user: store.state.user,
+		text: "",
+		title: "",
+		dest: ""
+		};
+	},
+	computed: {
+		marked: function() {
+		return marked(this.text, { sanitize: true });
+		}
+	},
+	methods: {
+		SendMessage: function() {
+		let sender, receiver;
 
-      MessagingService.sendMessage(sender, receiver, title, text)
-        .then(res => {})
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  },
-  mounted() {
-    store.watch(
-      state => {
-        return store.getters.getUserData;
-      },
-      (oldValue, newValue) => {
-        this.user = store.state.user;
-      }
-    );
-  }
-};
+		MessagingService.sendMessage(sender, receiver, title, text)
+			.then(res => {
+
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		}
+	},
+	mounted() {
+		store.watch(
+		state => {
+			return store.getters.getUserData;
+		},
+		(oldValue, newValue) => {
+			this.user = store.state.user;
+		}
+		);
+	}
+	};
 </script>
