9aaf105134f187bc919b213d5dbe764421c1a34d
Lós <los799@live.com>
2018-06-24 19:19:42 -0300

    Marca mensagem como lida apenas se usuário for destino

--- src/pages/ucp/messaging/message.vue
+++ src/pages/ucp/messaging/message.vue
@@ -39,6 +39,8 @@
 </template>
 
 <script>
+    import { store } from '@/vuex/store';
+
     import MessagingService from '@/services/messaging'
     import ForumService from '@/services/forum'
     import loader from 'vue-spinner/src/MoonLoader.vue'
@@ -50,6 +52,7 @@
     export default {
         data() {
             return {
+                user: store.state.user,
                 userAvatar: null,
                 userName: null,
                 message: [ ],
@@ -80,7 +83,12 @@
                     })
                 }
 
-                this.markAsRead(this.message._id);
+                if(this.message.receiver.username == this.user.username) {
+                    this.markAsRead(this.message._id);
+                } else {
+                    this.loading = false;
+                    this.$root.$emit('refreshNotReadedMessages');
+                }
             })
             .catch(error => {
                 console.log(error);
