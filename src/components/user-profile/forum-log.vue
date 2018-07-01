52ecf7729daa84035d565f3394984f39e1c46f0b
Lós <los799@live.com>
2018-07-01 14:18:18 -0300

    Correção em condicional para mostrar botão de logs do forum

--- src/components/user-profile/forum-log.vue
+++ src/components/user-profile/forum-log.vue
@@ -6,7 +6,7 @@
 				<div v-if="forum.processed">
 					<post v-for="post in lastPosts.slice(0, 5)" :key="post.id" :post="post" :user="user"/>
 				</div>
-				<a :href="'https://forum.pc-rpg.com.br/u/' + user.username" class="btn btn-primary" target="_blank" v-if="lastPosts.length >= 5">Ver todos</a>
+				<a :href="'https://forum.pc-rpg.com.br/u/' + user.username" class="btn btn-primary" target="_blank" v-if="lastPosts.length > 5">Ver todos</a>
 			</div>
 		</div>
 		<div v-else>
