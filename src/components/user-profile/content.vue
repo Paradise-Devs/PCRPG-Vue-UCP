b83a584655fff19e75e3f22e81384c804a2ec5b5
Lós <los799@live.com>
2018-11-04 14:00:09 -0200

    Define bio genérica

--- src/components/user-profile/content.vue
+++ src/components/user-profile/content.vue
@@ -1,37 +1,48 @@
 <template>
-    <div class="comp__userContent">
-        <h3 class="comp__userContent__username">{{ user.forumAtt.attributes.username }}</h3>
-        <p class="comp__userContent__bio" v-if="user.forumAtt.attributes.bio">{{ user.forumAtt.attributes.bio | bio }}</p>
-        <div class="tags">
-            <b-badge 
-                v-for="group in user.groups"
-                :key="group.id"
-                :style="{ borderColor: group.color }"
-            >
-                <span class="icon" :style="{ backgroundColor: group.color }"> 
-                    <icon :icon="['fas', group.icon]" />
-                </span>
-                <span :style="{ color: group.color }">{{ group.nameSingular}}</span>
-            </b-badge>
-        </div>
+  <div class="comp__userContent">
+    <h3 class="comp__userContent__username">{{ user.forumAtt.attributes.username }}</h3>
+    <p class="comp__userContent__bio" v-if="user.forumAtt.attributes.bio">{{ user.forumAtt.attributes.bio | bio }}</p>
+    <p class="comp__userContent__bio" v-else>Bio não definida.</p>
+    <div class="tags">
+      <b-badge 
+        v-for="group in user.groups"
+        :key="group.id"
+        :style="{ borderColor: group.color }"
+      >
+        <span class="icon" :style="{ backgroundColor: group.color }"> 
+          <icon :icon="['fas', group.icon]" />
+        </span>
+        <span :style="{ color: group.color }">{{ group.nameSingular}}</span>
+      </b-badge>
     </div>
+  </div>
 </template>
 
 <script>
-	import Vue from 'vue';
-	import { store } from '@/vuex/store';
-    
-    import { code, bolt, support, briefcase, pencilAlt } from '@fortawesome/fontawesome-free-solid';
+  import Vue from "vue";
+  import { store } from "@/vuex/store";
 
-    export default {
-        props: {
-            user: Object
-        },
-        filters: {
-            bio: function(text) {
-                let tmp = text.replace(/(?:\r\n|\r|\n)/g, ' ');
-                return tmp;
-            },
-        }
+  import {
+    code,
+    bolt,
+    support,
+    briefcase,
+    pencilAlt
+  } from "@fortawesome/fontawesome-free-solid";
+
+  export default {
+    props: {
+      user: Object
+    },
+    filters: {
+      bio: function(text) {
+        let tmp = text.replace(/(?:\r\n|\r|\n)/g, " ");
+        return tmp;
+      }
+    },
+    mounted() {
+      console.log(this.user.forumAtt.attributes.bio == null);
+      
     }
+  };
 </script>
