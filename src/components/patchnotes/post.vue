587529f12ae1d89ae1b9dacc9dc802282158b6a3
Lós <los799@live.com>
2018-11-18 17:32:13 -0200

    Melhorias para manutenção de posts

--- src/components/patchnotes/post.vue
+++ src/components/patchnotes/post.vue
@@ -1,47 +1,31 @@
 <template>
-    <div class="post">
-        <a :href="'https://forum.pc-rpg.com.br/d/' + patch.id + '-' + patch.attributes.slug" class="post__content">
-            <h3 class="post__content__title">{{ patch.attributes.title }}</h3>
-			<ul class="post__content__info">
-                <li class="post__created">
-                    <span>
-                        {{ patch.attributes.startTime | moment }}
-                    </span>
-                </li>
-            </ul>
-        </a>
-        <span class="post__comments"><icon :icon="['far', 'comments']" /> {{ patch.attributes.commentsCount }}</span>
-    </div>
+  <div class="post">
+    <a :href="'https://forum.pc-rpg.com.br/d/' + patch.id + '-' + patch.attributes.slug" class="post__content">
+      <h3 class="post__content__title">{{ patch.attributes.title }}</h3>
+      <ul class="post__content__info">
+        <li class="post__created">
+          <span>
+            {{ patch.attributes.startTime | moment }}
+          </span>
+        </li>
+      </ul>
+    </a>
+    <span class="post__comments"><icon :icon="['far', 'comments']" /> {{ patch.attributes.commentsCount }}</span>
+  </div>
 </template>
 
 <script>
-	import moment from 'moment';
+  import moment from "moment";
+  import { comments } from "@fortawesome/fontawesome-free-regular";
 
-    
-	import { comments } from '@fortawesome/fontawesome-free-regular';
-
-    export default {
-        props: {
-            patch: Object
-        },
-		filters: {
-			moment: function(time) {
-				return moment(time).fromNow();
-			}
-        }
+  export default {
+    props: {
+      patch: Object
+    },
+    filters: {
+      moment: function(time) {
+        return moment(time).fromNow();
+      }
     }
+  };
 </script>
-
-<style lang="scss">
-    .post {
-        padding-left: 20px;
-
-        .post__comments {
-            margin-right: -65px;
-
-            .fa-icon {
-                margin-left: -25px;
-            }
-        }
-    }
-</style>
