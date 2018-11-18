573145b0846f09cfd608e80cda3f7f345faf5192
Lós <los799@live.com>
2018-11-18 16:22:33 -0200

    Remove erro de firsttime do console

--- src/pages/ucp/model.vue
+++ src/pages/ucp/model.vue
@@ -9,75 +9,75 @@
 </template>
 
 <script>
-	import Vue from 'vue';
-	import { store } from '@/vuex/store';
-	import ucpmenu from '@/components/ucp-menu/block'
+import Vue from "vue";
+import { store } from "@/vuex/store";
+import ucpmenu from "@/components/ucp-menu/block";
 
-	export default {
-		data() {
-            return {
-				user: store.state.user,
-				firstTime: false,
-				userLoggedIn: null
-            }
-        },
-		watch: {
-			user: {
-				handler: function(val, oldVal) {
-					if(this.user.token == null) {
-						this.userLoggedIn = false;
-					} else {
-						this.userLoggedIn = true;
-					}
-				}, deep: true
-			}
-		},
-		computed: {
-			firstTime: function() {
-				var timeSave = localStorage.getItem('firstTimeUCP');
-				if(timeSave === "true" || timeSave === null) {
-					return true;
-				} else {
-					return false;
-				}
-			}
-		},
-		mounted() {
-			if(this.user.token == null) {
-				this.$router.push(this.$route.query.redirect || '/');
-			} else {
-				this.userLoggedIn = true;
-			}
+export default {
+  data() {
+    return {
+      user: store.state.user,
+      userLoggedIn: null
+    };
+  },
+  watch: {
+    user: {
+      handler: function(val, oldVal) {
+        if (this.user.token == null) {
+          this.userLoggedIn = false;
+        } else {
+          this.userLoggedIn = true;
+        }
+      },
+      deep: true
+    }
+  },
+  methods: {
+    firstTime: function() {
+      var timeSave = localStorage.getItem("firstTimeUCP");
+      if (timeSave === "true" || timeSave === null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  mounted() {
+    if (this.user.token == null) {
+      this.$router.push(this.$route.query.redirect || "/");
+    } else {
+      this.userLoggedIn = true;
+    }
 
-			store.watch(
-				(state)=>{
-					return store.getters.getUserData
-				},
-				(oldValue, newValue)=>{
-					this.user = store.state.user;
-				}
-			)
+    store.watch(
+      state => {
+        return store.getters.getUserData;
+      },
+      (oldValue, newValue) => {
+        this.user = store.state.user;
+      }
+    );
 
-			var timeSave = localStorage.getItem('firstTimeUCP');
-			if(timeSave === "true" || timeSave === null) {
-				this.firstTime = true;
-				localStorage.setItem('firstTimeUCP', true);
-			} else {
-				this.firstTime = false;
-			}
+    var timeSave = localStorage.getItem("firstTimeUCP");
+    if (timeSave === "true" || timeSave === null) {
+      this.firstTime = true;
+      localStorage.setItem("firstTimeUCP", true);
+    } else {
+      this.firstTime = false;
+    }
 
-			this.$root.$on('hideFirstTimeTut', (res) => {
-				this.firstTime = res;
-			});
-		},
-		components: {
-            ucpmenu
-		}
-	}
+    this.$root.$on("hideFirstTimeTut", res => {
+      this.firstTime = res;
+    });
+  },
+  components: {
+    ucpmenu
+  }
+};
 </script>
 
 <style lang="scss">
-	.ucp.container {
-		min-height: 60.8vh;
-	}
+.ucp.container {
+  min-height: 60.8vh;
+}
 </style>
