f7844d904e244c889f99c8181193b9a71d180860
Lós <los799@live.com>
2018-11-18 18:39:23 -0200

    Implementa versão mobile da página de desenvolvimento

--- src/pages/Development.vue
+++ src/pages/Development.vue
@@ -4,7 +4,29 @@
 			<spinner :loading="projectsLoading" color="#303846" size="90px"></spinner>
 			<b-row>
 				<b-col md="3">
-					<b-list-group v-if="projectsProcessed" class="list-group--custom">
+					<!-- mobile -->
+					<swiper 
+						:options="swiperOption" 
+						ref="branchesSwiper" 
+						class="list-group list-group--custom" 
+						v-if="isMobile && projectsProcessed"
+						@slideChangeTransitionEnd="swiped"
+					>
+						<swiper-slide 
+							v-for="project in projects" 
+							:key="project.id"
+							class="list-group-item list-group-item-action"
+							:data-projectid="project.id"
+						>
+							<h1 class="title">{{ project.name }}</h1>
+							<h3 class="desc">{{ project.description }}</h3>
+							<span class="stats"><b>Commits:</b> {{ project.commits }}</span>
+						</swiper-slide>
+						<div class="swiper-pagination" slot="pagination"></div>
+					</swiper>
+
+					<!-- desktop -->
+					<b-list-group v-if="projectsProcessed && !isMobile" class="list-group--custom">
 						<b-list-group-item
 							href="#"
 							v-for="project in projects"
@@ -18,7 +40,7 @@
 						</b-list-group-item>
 					</b-list-group>
 				</b-col>
-				<b-col md="9">
+				<b-col md="9" class="commits__container">
 					<div infinite-wrapper>
 						<commits 
 							v-for="commit in commits" 
@@ -37,12 +59,13 @@
 </template>
 
 <script>
-	import GitService from '@/services/gitlab'
-	import InfiniteLoading  from 'vue-infinite-loading';
-	import spinner from 'vue-spinner/src/MoonLoader.vue';
+	import GitService from "@/services/gitlab";
+	import InfiniteLoading from "vue-infinite-loading";
+	import spinner from "vue-spinner/src/MoonLoader.vue";
+	import { swiper, swiperSlide } from "vue-awesome-swiper";
 
-	import commits from '@/components/commits/Commit.vue'
-	import { setTimeout } from 'timers';
+	import commits from "@/components/commits/Commit";
+	import { setTimeout } from "timers";
 
 	export default {
 		data() {
@@ -52,13 +75,35 @@
 				totalcommits: 2,
 				projectsProcessed: false,
 				projectsLoading: true,
-				commits: [ ],
-				projects: [ ],
-				defaultBranch: '',
+				commits: [],
+				projects: [],
+				defaultBranch: "",
 				selectedProject: 0,
 				infiniteId: +new Date(),
-				stats: [ ]
-			}
+				stats: [],
+
+				swiperOption: {
+					slidesPerView: 1,
+					init: true,
+					variableWidth: false,
+					pagination: {
+							el: '.swiper-pagination',
+					}
+				},
+				previousSlider: 0
+			};
+		},
+		computed: {
+			isMobile: function() {
+				if (window.innerWidth < 768) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+			swiper() {
+        return this.$refs.branchesSwiper.swiper
+      }
 		},
 		methods: {
 			activeProject(project) {
@@ -69,28 +114,31 @@
 				this.getCommits();
 			},
 			getCommits() {
-				return GitService.getProjectCommits(this.selectedProject, this.currentPage, this.defaultBranch);
+				return GitService.getProjectCommits(
+					this.selectedProject,
+					this.currentPage,
+					this.defaultBranch
+				);
 			},
 			updateScroller($state) {
 				this.getCommits()
-				.then(response => {
-					if(response.data.length > 0) {
-						this.currentPage++;
-						this.commits = this.commits.concat(response.data);
+					.then(response => {
+						if (response.data.length > 0) {
+							this.currentPage++;
+							this.commits = this.commits.concat(response.data);
+							$state.loaded();
+						} else {
+							$state.complete();
+						}
+					})
+					.catch(e => {
+						console.log(e);
 						$state.loaded();
-					} else {
-						$state.complete();
-					}
-				})
-				.catch(e => {
-					console.log(e);
-					$state.loaded();
-				});
+					});
 			},
 			getStats() {
-				for(let i in this.projects) {
-					GitService.getStats(this.projects[i].id)
-					.then(response => {
+				for (let i in this.projects) {
+					GitService.getStats(this.projects[i].id).then(response => {
 						this.projects[i].commits = response.data.statistics.commit_count;
 					});
 				}
@@ -100,24 +148,41 @@
 					self.projectsProcessed = true;
 					self.projectsLoading = false;
 				}, 1000);
+			},
+			swiped() {
+				let el = document.querySelectorAll('.list-group--custom .swiper-slide');
+				let s = this;
+				for(let i in el) {
+					if(el[i].classList.contains('swiper-slide-active')) {
+						if(i != this.previousSlider) {
+							this.previousSlider = i;
+							this.activeProject(el[i].dataset.projectid);
+						}
+						break;
+					}
+				}
 			}
 		},
-		
+
 		mounted() {
 			GitService.getProjects()
-			.then(response => {
-				this.projects = response.data;
-				this.selectedProject = this.projects[0].id;
-				this.defaultBranch = this.projects[0].default_branch;
-				this.getStats();
-			})
-			.catch(e => {
-				console.log(e);
-				$state.loaded();
-			});
+				.then(response => {
+					this.projects = response.data;
+					this.selectedProject = this.projects[0].id;
+					this.defaultBranch = this.projects[0].default_branch;
+					this.getStats();
+				})
+				.catch(e => {
+					console.log(e);
+					$state.loaded();
+				});
 		},
 		components: {
-			InfiniteLoading, commits, 'spinner': spinner
-		},
-	}
+			InfiniteLoading,
+			commits,
+			spinner: spinner,
+			swiper,
+    	swiperSlide
+		}
+	};
 </script>
